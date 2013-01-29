df63babbc84bf4b9ca43282e441ea0cb7515f575
Thomas Lackner <tom@go.co>
2013-01-29 15:33:40 -0600

    use cluster module and remove express (unnecessary for this service)

--- src/server.js
+++ src/server.js
@@ -1,53 +1,73 @@
 var config = require('../config'),
+		cluster = require('cluster'),
 		fs = require('fs'),
 		http = require('http'),
-		express = require('express'),
-		app = express();
+		url = require('url'),
+		taken = JSON.stringify({ status: 'taken' }),
+		avail = JSON.stringify({ status: 'avail' }),
+		hitStats = {}, 
+		nHits = 0,
+		domainData;
+
+function checkName(data) {
+
+	return function(req, res) { 
+		var path = url.parse(req.url).pathname.substr(1);
+		
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		var name = path.toLowerCase();
+		if (name.substr(-3) === '.CO')
+			name = name.substr(0, -3);
+		// console.log(name);
+		if (name in data)
+			res.write(taken);
+		else
+			res.write(avail);
+		res.end();
+		nHits++;
+		if (nHits % 100 == 0)
+			console.log('nHits for #'+ cluster.worker.id, nHits);
+	}
+}
 
 function initData(fn) {
 	return JSON.parse(fs.readFileSync(fn));
 }
 
-function initExpress(httpPort, domainData) {
-	app.configure(function(){
-		app.set('port', process.env.PORT || httpPort);
-		app.use(express.favicon());
-		app.use(express.logger('dev'));
-		app.use(express.bodyParser());
-		app.use(express.methodOverride());
-		app.use(app.router);
-	});
-	app.configure('development', function(){
-		app.use(express.errorHandler());
-	});
-	app.get('/mu-8313f9bf-9c03f615-cce7738e-86c841dd', function(req, res) {
-		res.send('42');
-	});
-	app.get('/:name', function(req, res){
-		var name = req.param('name').toLowerCase();
-		if (name.substr(-3) === '.CO')
-			name = name.substr(0, -3);
-		console.log(name);
-		if (name in domainData)
-			res.send({ status: 'taken' });
-		else
-			res.send({ status: 'avail' });
-	});
+function initExpress(httpPort, data) {
+	var port = process.env.PORT || httpPort;
+	/*
 	http.createServer(app).listen(app.get('port'), function(){
 		var nKeys = Object.keys(domainData).length;
 		console.log("Express server listening on port " + app.get('port') + " for " + nKeys + " domains");
 	});
+	*/
+	http.createServer(checkName(data)).listen(port);
+	console.log('worker '+cluster.worker.id+' listening');
 }
 
 function init() {
+	var nCpus = require('os').cpus().length;
+
 	if (process.argv.length != 3) {
 		console.error('usage: node server.js [zonedata.json]');
 		process.exit(1);
 	}
-	console.log('config', config);
-	console.log('argv', process.argv);
-	var data = initData(process.argv[2]);
-	initExpress(config.httpPort, data);
+
+	if (cluster.isMaster) {
+		console.log('config', config);
+		console.log('argv', process.argv);
+		for (var i = 0; i < nCpus; i++) {
+			cluster.fork();
+		}
+		cluster.on('exit', function(worker, code, signal) {
+			console.log('worker '+worker.process.pid+' died');
+		});
+	} else {
+		domainData = initData(process.argv[2]);
+		initExpress(config.httpPort, domainData);
+	}
+
 }
 
 init();
